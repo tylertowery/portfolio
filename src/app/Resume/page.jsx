29c8a7befc9cf5b42d73e5d766d604de7487f5ae
Tyler Towery <tylertowery21@gmail.com>
2024-09-11 14:27:55 -0700

    adjusting size of image

--- src/app/Resume/page.jsx
+++ src/app/Resume/page.jsx
@@ -4,7 +4,13 @@ export default function Resume() {
 
   return (
     <div className='resume-container'>
-      <iframe src='/TylerToweryResume.pdf' width='100%' height='100%' title='Tyler Towery Resume' />
+      <iframe
+        src='/TylerToweryResume.pdf'
+        width='100%'
+        height='100%'
+        title='Tyler Towery Resume'
+        style={{ border: 'none' }}
+      />
     </div>
   )
 }
